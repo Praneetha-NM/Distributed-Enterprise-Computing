73b340d89784442dc48fc81addaffe62402e8582
Praneetha-NM <praneetha7597@gmail.com>
2025-01-02 09:14:59 +0530

    CHANGE PS-2

--- PS-2/Q1/server.js
+++ PS-2/Q1/server.js
@@ -1,8 +1,8 @@
+
+
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-
-
 
 
 const app = express();
@@ -10,33 +10,23 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 
-
-
 app.use(express.static('public'));
 
 
-
-
 io.on('connection', (socket) => {
-  console.log('A user connected');
-  socket.on('chat message', (data) => {
-      io.emit('chat message', data);
-  });
+   console.log('A user connected');
+   socket.on('chat message', (data) => {
+       io.emit('chat message', data); 
+   });
 
 
-
-
-  socket.on('disconnect', () => {
-      console.log('A user disconnected');
-  });
+   socket.on('disconnect', () => {
+       console.log('A user disconnected');
+   });
 });
-
-
 
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
